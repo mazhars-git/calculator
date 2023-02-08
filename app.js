b1af18f4a38ff1cea3139b964f368beb61635f12
Md. Mazharul Islam <mazharulislam611@gmail.com>
2023-02-08 10:48:50 +0600

    upadeat

--- app.js
+++ app.js
@@ -9,8 +9,24 @@
         button.addEventListener('click', function(e){
             let value = e.target.dataset.num;
             screen.value += value;
-            console.log(value)
         })
        
+    });
+
+    equal.addEventListener('click', function(e){
+        if (screen.value == '') {
+            screen.value = ""
+        }else{
+            let answer = eval(screen.value);
+            screen.value = answer;
+        }
     })
+
+    clear.addEventListener('click', function(e) {
+        screen.value = '';
+        
+    })
+
+
+
 })();
